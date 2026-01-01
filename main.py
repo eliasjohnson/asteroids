aa3edb1e2c27f8ce81ffe65d8f79f3e494bf5768
Elias Johnson <eliasjohnson211@gmail.com>
2026-01-01 16:26:04 -0700

    added sprites class

--- main.py
+++ main.py
@@ -9,18 +9,27 @@ def main():
     
     #start game
     p.init()
+    clock = p.time.Clock()
+    dt = 0
+    frame_rate = 60
     screen = p.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
     
     #endless game loop, only exit on close window event (currently)
     while True:
         log_state()
+        
         # will check for close window event, making the window close button work!
         for event in p.event.get():
             if event.type == p.QUIT:
                 return
         screen.fill("black")
         p.display.flip()
+        clock.tick(frame_rate)
 
+        # delta time, delta being change in value. in gaming, delta time(dt) is
+        # "time that has passed since the last frame was drawn".
+        dt = clock.tick(frame_rate) / 1000
+        
 
 if __name__ == "__main__":
     main()
