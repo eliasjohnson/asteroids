02bb137641b1bb8ee32ec4b3d1d21033edb7e136
Elias Johnson <eliasjohnson211@gmail.com>
2026-01-05 16:20:47 -0700

    asteroid field and circle shape classes

--- main.py
+++ main.py
@@ -2,25 +2,43 @@ import pygame as p
 import constants as c
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
+    # groups
+    asteroids = p.sprite.Group()
+    updatable = p.sprite.Group()
+    drawable = p.sprite.Group()
+
+    # Player is the name of the class, not an instance of it
+    # This must be done before any Player objects are created
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    
     print(f"Starting Asteroids with pygame version: {p.version.ver}!")
     print(f"Screen width: {c.SCREEN_WIDTH} \nScreen height: {c.SCREEN_HEIGHT}")
-    
-    #start game
+
+    # start game
     p.init()
     clock = p.time.Clock()
     dt = 0
     frame_rate = 60
     screen = p.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
-    #actual player
-    player_ship = Player(x=c.SCREEN_WIDTH / 2, y=c.SCREEN_HEIGHT / 2,  radius=c.PLAYER_RADIUS) 
+    
+    # actual player
+    player_ship = Player(
+        x=c.SCREEN_WIDTH / 2, y=c.SCREEN_HEIGHT / 2, radius=c.PLAYER_RADIUS
+    )
+    
+    asteroid_f = AsteroidField()
 
-    #endless game loop, only exit on close window event (currently)
+    # endless game loop, only exit on close window event (currently)
     while True:
         log_state()
-        
+
         # will check for close window event, making the window close button work!
         for event in p.event.get():
             if event.type == p.QUIT:
@@ -31,14 +49,20 @@ def main():
         # delta time, delta being change in value. in gaming, delta time(dt) is
         # "time that has passed since the last frame was drawn".
         dt = clock.tick(frame_rate) / 1000
-        
 
-        #rotate ship
-        player_ship.update(dt)
-        
-        #render
-        player_ship.draw(screen)
+        # rotate ship
+        # updates the group "updatable" of all updatable objects
+        updatable.update(dt)
+
+        # render
+        for item in drawable:
+            item.draw(screen)
+            
         p.display.flip()
+
+        # update() moves the objects in your data, Changes the game state: positions, velocities, health, timers, etc.
+        # draw() paints that data to the off-screen buffer by reading the state (like player.position) and draws sprites at those coordinates onto the screen surface.
+        # and flip() shows that buffer. Makes whatever draw() rendered actually appear on the monitor.
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -37,7 +37,7 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(dt * -1)
-    
+
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
